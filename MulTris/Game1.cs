a44f839af137219e3c75c9f928517f125be50d9b
SkillGG <github@skilletss.cba.pl>
2019-03-02 03:40:39 +0100

    First Load

---
+++ MulTris/Game1.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace MulTris {
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game {
+
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		public enum GameState {
+			MENU,
+			T4,
+			T5,
+			T6,
+			T7,
+			T8,
+			OPTIONS
+		}
+
+		public GameState gamestate = GameState.MENU;
+
+		public Point ScreenCentre(int W) {
+			return new Point(( GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - W ) / 2, 0);
+		}
+
+		public Tile tile1;
+
+		public static int WIDTH = 600, HEIGHT = 700;
+
+		public Game1() {
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+
+			// Show the mouse
+			this.IsMouseVisible = true;
+
+			tile1 = new Tile(null, Color.White);
+
+			// Setting default resolution settings
+			this.graphics.PreferredBackBufferHeight = HEIGHT;
+			this.graphics.PreferredBackBufferWidth = WIDTH;
+			this.graphics.IsFullScreen = false;
+			Window.Position = ScreenCentre(WIDTH);
+			Window.IsBorderless = true;
+
+
+			// Setting default FPS settings
+			this.IsFixedTimeStep = true;
+			this.graphics.SynchronizeWithVerticalRetrace = true;
+			this.TargetElapsedTime = new System.TimeSpan(0, 0, 0, 0, 33); // Play at ~30.3FPS
+
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize() {
+			// TODO: Add your initialization logic here
+
+			base.Initialize( );
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent() {
+			// Create a new SpriteBatch, which can be used to draw textures.
+			this.spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			this.tile1.Load(Content, "T5/TileTXT", new Rectangle(0, 0, 50, 50));
+
+			// TODO: use this.Content to load your game content here
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent() {
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime) {
+			if( GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState( ).IsKeyDown(Keys.Escape) )
+				Exit( );
+
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime) {
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// TODO: Add your drawing code here
+
+			// Start collecting .Draw(...) calls 
+			this.spriteBatch.Begin( );
+
+			if( this.gamestate == GameState.MENU ) {
+				// RENDER ALL MENU SPRITES
+				this.tile1.Draw(this.spriteBatch);
+
+			}
+			// Render everything on screen
+			this.spriteBatch.End( );
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ MulTris/Tile.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+
+namespace MulTris {
+	public class Tile {
+		/// <summary>
+		///		Whole Texture Sprite
+		/// </summary>
+		Texture2D texture;
+
+		/// <summary>
+		///		Source Rectangle
+		/// </summary>
+		Rectangle SR;
+
+		/// <summary>
+		///		Tint color for this tile
+		/// </summary>
+		Color color;
+
+		/// <summary>
+		///		Draw boundaries
+		/// </summary>
+		Rectangle bound;
+
+		/// <summary>
+		/// Flag, is texture loaded
+		/// </summary>
+		private bool loaded;
+
+		/// <summary>
+		/// Flag, has error message been showed already. Anti-spam system
+		/// </summary>
+		private bool ems = false;
+
+		/// <summary>
+		/// Blocking a default constructor
+		/// </summary>
+		private Tile() { }
+
+		public Tile(Nullable<Rectangle> bn, Color inColor) {
+			color = inColor;
+			bound = bn ?? new Rectangle(0, 0, 50, 50);
+			loaded = false;
+		}
+
+		/// <summary>
+		/// Moves tile to <paramref name="x"/>, <paramref name="y"/> coordinates.
+		/// </summary>
+		/// <param name="x">X coordinate</param>
+		/// <param name="y">Y coordinate</param>
+		public void Move(int x, int y) {
+			bound.X = x;
+			bound.Y = y;
+		}
+
+		/// <summary>
+		/// Moves tile by <paramref name="x"/> in X-axis and <paramref name="y"/> in Y-axis.
+		/// </summary>
+		/// <param name="x">move in X-axis</param>
+		/// <param name="y">move in Y-axis</param>
+		public void MoveBy(int x, int y) {
+			bound.X += x;
+			bound.Y += y;
+		}
+
+		/// <summary>
+		/// Scales(changes size) of the sprite by a factor of <paramref name="scale"/>. This function scales both dimensions of the sprite.
+		/// </summary>
+		/// <param name="scale">Factor to scale by</param>
+		public void Scale(float scale) {
+			bound.Width = (int) ( ( (float) bound.Width ) * scale );
+			bound.Height = (int) ( ( (float) bound.Width ) * scale );
+		}
+
+		/// <summary>
+		/// <para>
+		/// Loads a texture from ContentManager via <see cref="ContentManager.Load{T}(string)"/>
+		/// </para>
+		/// </summary>
+		/// <param name="cm"><see cref="ContentManager"/> to Load texture from</param>
+		/// <param name="str">A String that will be fed into <see cref="ContentManager.Load{T}(string)"/>.</param>
+		/// <param name="source">A part of texture that will be taken from the whole image. Leave <see langword="null"/> if the whole texture should be loaded</param>
+		public void Load(ContentManager cm, String str, Nullable<Rectangle> source) {
+			texture = cm.Load<Texture2D>(str);
+			SR = source ?? texture.Bounds;
+			loaded = true;
+		}
+
+		/// <summary>
+		/// Draws the tile
+		/// </summary>
+		/// <param name="sb"><see cref="SpriteBatch"/> to draw texture in.</param>
+		public void Draw(SpriteBatch sb) {
+			try {
+				if( loaded )
+					sb.Draw(texture, bound, SR, color);
+				else
+					throw new ArgumentNullException("Tile has not been loaded yet!");
+			} catch( ArgumentNullException e ) {
+				if( !this.ems )
+					Console.WriteLine("NullException: " + e.Message + " @ " + e.StackTrace);
+				this.ems = true;
+			} catch( Exception e ) {
+				if( !this.ems )
+					Console.WriteLine("Unexpected Error: " + e.Message + " @ " + e.StackTrace);
+				this.ems = true;
+			}
+		}
+
+		~Tile() {
+			if( this.texture != null )
+				this.texture.Dispose( );
+			this.texture = null;
+		}
+	}
+
+}
