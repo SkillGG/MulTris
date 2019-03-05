1114e7f56aa7351781644598ba818040f89d1663
SkillGG <github@skilletss.cba.pl>
2019-03-05 19:04:46 +0100

    BIG BUG FIX
    ======
    Bug: KeyUp and KeyDown functions don't catch properly
    Everytime KeyDown was fired one .Update after KeyUp fired even though the state of the key is not change to earlier one.
    
    Fix: Now KeyUp and KeyDown check if KeyChange and then check which state is button in now!
    Added KeyChange to check if key state changed, but without checking what button was clicked.
    =====
    
    Added every needed command to handle gamePad clickes and changes!
    TODO: Add gamePad 8-axis easier support.
    
    Signed-off-by: SkillGG <github@skilletss.cba.pl>

--- MulTris/InputState.cs
+++ MulTris/InputState.cs
@@ -40,9 +40,11 @@ namespace MulTris {
 		public GamePadState gamePad1;
 		public GamePadState gamePad2;
 
-		public Rectangle MouseRectangle { get {
+		public Rectangle MouseRectangle {
+			get {
 				return new Rectangle(mouse.Position, new Point(1, 1));
-		} }
+			}
+		}
 
 		public InputState() {
 			this.mouse = Mouse.GetState( );
@@ -58,10 +60,110 @@ namespace MulTris {
 			this.gamePad2 = GamePad.GetState(1);
 		}
 
-		public bool StateChangeDown(InputState b, Keys k) {
+
+		// KEYS
+
+		public bool IsKeyDown(Keys k) { return this.keyboard.IsKeyDown(k); }
+		public bool IsKeyUp(Keys k) { return this.keyboard.IsKeyUp(k); }
+
+		// KeyChange
+		public bool KeyChange(InputState b, Keys k) {
 			this.Update( );
 			if( b.keyboard.IsKeyDown(k) != this.keyboard.IsKeyDown(k) )
 				return true;
+			return false;
+		}
+
+		// KeyUp
+		public bool KeyUp(InputState b, Keys k) {
+			if( KeyChange(b, k) )
+				if( IsKeyUp(k) )
+					return true;
+			return false;
+		}
+
+		// KeyDown
+		public bool KeyDown(InputState b, Keys k) {
+			if( KeyChange(b, k) )
+				if( IsKeyDown(k) )
+					return true;
+			return false;
+		}
+
+
+		// BUTTONS
+
+		// IsButtonDown
+		public byte IsButtonDown(Buttons k) {
+			this.Update( );
+			if( gamePad1.IsButtonDown(k) ) {
+				if( gamePad2.IsButtonDown(k) )
+					return 3;
+				return 1;
+			} else if( gamePad2.IsButtonDown(k) )
+				return 2;
+			return 0;
+		}
+		public bool IsButtonDownAny(Buttons k) {
+			return ( IsButtonDown(k) != 0 );
+		}
+
+		// IsButtonUp
+		public byte IsButtonUp(Buttons k) {
+			this.Update( );
+			if( gamePad1.IsButtonUp(k) ) {
+				if( gamePad2.IsButtonUp(k) )
+					return 3;
+				return 1;
+			} else if( gamePad2.IsButtonUp(k) )
+				return 2;
+			return 0;
+		}
+		public bool IsButtonUpAny(Buttons k) {
+			return ( IsButtonUp(k) != 0 );
+		}
+
+		// Button Change
+		public byte ButtonChange(InputState b, Buttons k) {
+			this.Update( );
+			if( b.gamePad1.IsButtonDown(k) != this.gamePad1.IsButtonDown(k) ) {
+				if( b.gamePad2.IsButtonDown(k) != this.gamePad2.IsButtonDown(k) )
+					return 3;
+				return 1;
+			}
+			if( b.gamePad2.IsButtonDown(k) != this.gamePad2.IsButtonDown(k) )
+				return 2;
+			return 0;
+		}
+		public bool ButtonChangeAny(InputState b, Buttons k) {
+			return ( ButtonChange(b, k) != 0 );
+		}
+
+		// ButtonDown
+		public byte ButtonDown(InputState b, Buttons k) {
+			byte bc = ButtonChange(b, k);
+			if( bc != 0 && bc == IsButtonDown(k) )
+				return bc;
+			return 0;
+		}
+		public bool ButtonDownAny(InputState b, Buttons k) {
+			if( ButtonChangeAny(b, k) )
+				if( IsButtonDownAny(k) )
+					return true;
+			return false;
+		}
+
+		// ButtonUp
+		public byte ButtonUp(InputState b, Buttons k) {
+			byte bc = ButtonChange(b, k);
+			if( bc != 0 && bc == IsButtonUp(k) )
+				return bc;
+			return 0;
+		}
+		public bool ButtonUpAny(InputState b, Buttons k) {
+			if( ButtonChangeAny(b, k) )
+				if( IsButtonUpAny(k) )
+					return true;
 			return false;
 		}
 
